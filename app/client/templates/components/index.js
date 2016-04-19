b57711d54dda762c6515d709994beb58536f8864
YusukeMukoyama <yusuke.mukoyama@gmail.com>
2016-04-19 18:46:20 +0900

    editing index.js/index.html
    In progress

--- app/client/templates/components/index.js
+++ app/client/templates/components/index.js
@@ -1,5 +1,18 @@
-Template.index.helpers({
-  accounts: function(){
-    return EthAccounts.find({});
+Template.components_index.helpers({
+//  accounts: function(){
+//    return EthAccounts.find({});
+//  }
+  contracts: function(){
+    { _id: 100 }; // 動作テスト用
+//    return Contracts.find({}); //Contracts collectionから全て取得
   }
+});
+
+// 購入ボタンを押した時のイベント
+Template.components_index.events({
+    'submit .btn-success': function(event) {
+    event.preventDefault();
+    // get the id(account, abi) of pushed
+    // send transaction to contract by using web3
+    }
 });
