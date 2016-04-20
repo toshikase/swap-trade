6baa5778c0bc2df8dd0f4efe1618db3a12efba77
YusukeMukoyama <yusuke.mukoyama@gmail.com>
2016-04-20 12:08:51 +0900

    get clicked button id by using jquery / send tx

--- app/client/templates/components/index.js
+++ app/client/templates/components/index.js
@@ -17,11 +17,12 @@ Template.components_index.events({
     'click .btn-success': function(event) {
     event.preventDefault();
     var abi = MultiplyContract.abi;
-    var contract_address = "0xcf69fcc3c34933191c54de8901d3c440db6d74fc";
+    // get the contract_address
+    var contract_address = $(event.target).attr('id');
+    console.log(contract_address);
+    //var contract_address = "0xcf69fcc3c34933191c54de8901d3c440db6d74fc";
     var cnt = web3.eth.contract(abi).at(contract_address);
     var result = cnt.Respond.sendTransaction(web3.eth.accounts[0], 100000, {from: web3.eth.accounts[0], gas:500000});
     alert( "購入が完了しました!!\nトランザクションアドレスは" + result ); 
-    // get the id(account, abi) of pushed
-    // send transaction to contract by using web3
     }
 });
