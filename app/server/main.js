3bdc8ba2a28f5db260c99707f628ff41eb827f08
t.kaseda <kaseda@sendee.jp>
2016-05-03 15:50:27 +0900

    solved conflict

--- app/server/main.js
+++ app/server/main.js
@@ -2,21 +2,6 @@ if (Meteor.isServer) {
   Meteor.startup(function () {
 
     Meteor.methods({
-      'insert_contracts': function(address, price, amount, exeday, premium, position, contract_address, abi){
-         Contracts.insert({
-           seller: address,
-           price: price,
-           amount: amount,
-           exeday: exeday,
-           premium: premium,
-           position: position,
-           contract_address: contract_address,
-           abi: abi
-         });
-      }
-    });
-
-    Meteor.methods({
       'insert_transactions': function(address, price, amount, exeday, premium, position, contract_address, abi, buy_date, done){
          Transactions.insert({
            seller: address,
@@ -32,14 +17,16 @@ if (Meteor.isServer) {
          });
       }
     });
-// 
-// 下記メソッドを書いて、クライアント側から呼び出しても上手くいかない
-// コンソールから見ると、サーバー側では取得できている??
-//    Meteor.methods({
-//      'find_contracts': function(){
-//        console.log("called find_contracts method");
-//        return Contracts.find({});
-//    }
-//  });
+
+  Meteor.publish('contracts', function contractsPublication() {
+    return Contracts.find();
+  });
+
+Meteor.methods({
+     'find_contracts': function(){
+       console.log("called find_contracts method");
+       return Contracts.find({});
+   }
+ });
 });
 };
