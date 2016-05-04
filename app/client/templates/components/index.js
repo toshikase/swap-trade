c03d2da441d8cb056fb83c0956cd637a79538a20
YusukeMukoyama <yusuke.mukoyama@gmail.com>
2016-05-04 12:37:59 +0900

    一時コミット

--- app/client/templates/components/index.js
+++ app/client/templates/components/index.js
@@ -15,7 +15,7 @@ Template.contract.events({
     // get abi & contract_address
     var abi = MultiplyContract.abi;
     var contract_address = $(event.target).attr('id');
-    //send transaction to contract
+    // send transaction to contract
     // accountはunlockしていないとエラーになる
     var cnt = web3.eth.contract(abi).at(contract_address);
     var result = cnt.Respond.sendTransaction(web3.eth.accounts[0], 100000, {from: web3.eth.accounts[0], gas:500000});
@@ -23,8 +23,8 @@ Template.contract.events({
 
     // 以下、transaction collection機能の実装
     var buy_date = new Date(); // 購入日データ
-    var done = "false";
-    var hogehge // Mongoから購入した商品のデータを取得してくる
+    var done = "false";  //実行されたらtrueになる
+    var address = 
     Meteor.call('insert_transactions', address, price, amount, exeday, premium, position, contract_address, abi, buy_date, done); // Mongoに格納
 
   }
