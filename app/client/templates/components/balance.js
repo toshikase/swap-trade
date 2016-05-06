7313679872f7fcfd6efd2a9b133b03feada499c8
t.kaseda <kaseda@sendee.jp>
2016-05-06 13:24:48 +0900

    done

--- app/client/templates/components/balance.js
+++ app/client/templates/components/balance.js
@@ -11,17 +11,6 @@ The balance template
 @constructor
 */
 
-SyncedCron.add({
-  name: 'Crunch some important numbers for the marketing department',
-  schedule: function(parser) {
-    // parser is a later.parse object
-    return parser.text('every 2 hours');
-  },
-  job: function() {
-    var numbersCrunched = CrushSomeNumbers();
-    return numbersCrunched;
-  }
-});
 
 // when the template is rendered
 Template['components_balance'].onRendered(function() {
